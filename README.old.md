d4580f47395f5239a8c1f602678a1278d38cda68
Jérémy <kirijeremy@gmail.com>
2022-07-12 12:01:02 +0200

    Start new project

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# Bandidos
---
+++ src/components/Concept.jsx
@@ -0,0 +1,19 @@
+import paysage from "../img/firstimg.png"
+
+const Concept = () => {
+    return ( <div className="generalPres"> 
+    <div className="firstSection">
+        <div className="description">
+            <h1>Bandidos</h1>
+            <h2>Trouver votre prime</h2>
+            <p>Eh Amigo des Hautes Plaines aux canyons du Grand Labyrinthe, l’Ouest est vaste et t'as du mal à trouver une nouvelle prime ? En ces temps difficiles ça se comprend ! Mais heureusement nous sommes là. </p>
+        </div>
+        <div className="illustration">
+            <img src={paysage} alt="paysage"/>
+        </div>
+    </div>
+    <div className="lastSection"></div>
+
+    </div>) 
+    }
+    export default Concept
