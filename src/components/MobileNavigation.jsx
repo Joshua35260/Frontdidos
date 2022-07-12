f29093716ae09cffb9b4db9fc4f6f2595cf54833
Alexandra Panastier <panastier.alexandra@gmail.com>
2022-07-13 01:58:41 +0200

    new change add fatigue

---
+++ src/components/MobileNavigation.jsx
@@ -0,0 +1,11 @@
+import Header from './Header'
+
+const MobileNavigation = () => {
+  return (
+    <nav className={classes.MobileNavigation}>
+        <Header />
+    </nav>
+  )
+};
+
+export default MobileNavigation;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import NavLinks from "./NavLinks";
+
+const Navigation = () => {
+  return (
+    <nav className={classes.Navigation}>
+        <NavLink />
+    </nav>
+  )
+};
+
+export default Navigation;
