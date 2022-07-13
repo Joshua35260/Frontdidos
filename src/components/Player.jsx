be45a95bf4ef4b7cdb350c07519f5604c153bad7
Alexandra Panastier <panastier.alexandra@gmail.com>
2022-07-13 09:25:29 +0200

    Bandidos pan pan !

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,15 @@
 import { ImPlay3, ImPause2} from "react-icons/im";
+import "./style/Player.css"
 
-const Player = ({audioElem, isplaying, setisplaying}) => {
+const Player = ({ isplaying, setisplaying, play, setPlay}) => {
 
     const PlayPause = () => {
         setisplaying(!isplaying)
+        setPlay(!play)
     }
     return (
         <div className="controls">
-    <ImPlay3 className="btn-action" />
+    {isplaying ? <ImPause2 className="btn-action" onClick={PlayPause} /> : <ImPlay3 className="btn-action" onClick={PlayPause} /> }
         </div>
     )
 }
--- src/components/style/audio/audio.js
+++
@@ -1,6 +0,0 @@
-exports.songdata = [
-{
-    "title": "Western - Brett Van Donsel",
-    "url":"https://www.youtube.com/watch?v=6FrlCfkffVI"
-}
-]
